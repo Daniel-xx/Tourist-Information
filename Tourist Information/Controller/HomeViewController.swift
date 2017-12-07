ea54fd298aeb9f15853f55d1298af0fb18dbb2bd
Daniel-xx <danieltravers78@gmail.com>
2017-12-07 19:36:09 +0000

    Main Menu Complete
    No code - Just design work. All main window is finished ... Thats all folks

--- Tourist Information/Controller/HomeViewController.swift
+++ Tourist Information/Controller/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Tourist Information
 //
 //  Created by Daniel Travers on 07/12/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Tourist Information/Controller/InfoMenuViewController.swift
@@ -0,0 +1,26 @@
+//
+//  InfoMenuViewController.swift
+//  Tourist Information
+//
+//  Created by Daniel Travers on 07/12/2017.
+//  Copyright © 2017 Daniel Travers. All rights reserved.
+//
+
+import UIKit
+
+class InfoMenuViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
